7552f7862ecb62161a5fa4dbe41ef1516bbba163
Roman Ledjajev <roledxd@gmail.com>
2023-04-03 19:42:57 +0300

    huy

--- server.js
+++ server.js
@@ -24,27 +24,35 @@ app.all('/*', function (req, res, next) {
   res.header("Access-Control-Allow-Methods", "GET");
   res.header("Access-Control-Allow-Headers", "X-Requested-With,Content-type,Accept,X-Access-Token,X-Key");
 
+  res.contentType('text/html');
+
+  next();
+});
+
+app.all('/server/*', function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With,Content-type,Accept,X-Access-Token,X-Key");
+
   res.contentType('application/json');
 
   next();
 });
 
+
 app.get('/', (req, res) => {
-  res.send('API')
+  res.send('now playing: ' + onlineCount())
 })
 
-app.get('/serverdata', (req, res) => {
+app.get('/server', (req, res) => {
   res.json({
     address: req.get('host') + ':' + wsport,
     mnt_msg: "Server is under maintenance, try again later.",
     is_mnt: false,
     name: "Main server",
-    motd: "TESTING"
+    motd: "TESTING",
+    online: onlineCount()
   })
-})
-
-app.post('/signin', (req, res) => {
-  res.send('auth req')
 })
 
 app.listen(port, () => {
@@ -56,31 +64,47 @@ const wss = new WebSocket.Server({ port: wsport });
 
 const clients = new Map();
 
+const planets = new Map();
+
+function onlineCount() {
+  return clients.entries.length;
+}
+
 wss.on('connection', (ws) => {
   const id = uuidv4();
-  const color = Math.floor(Math.random() * 360);
-  const metadata = { id, color };
+  const signedIn = false;
+  const nickname = null;
+  const ip = null;
+  const playerdata = { id, signedIn, nickname, ip };
 
-  clients.set(ws, metadata);
+  clients.set(ws, playerdata);
+
   ws.on('message', (messageAsString) => {
-    const message = JSON.parse(messageAsString);
-    const metadata = clients.get(ws);
 
-    message.sender = metadata.id;
-    message.color = metadata.color;
+    const message = JSON.parse(messageAsString);
+    const playerdata = clients.get(ws);
+
+    message.sender = playerdata.id;
+    message.signedIn = playerdata.signedIn;
+    message.nickname = playerdata.nickname;
+    message.ip = playerdata.ip;
+
     const outbound = JSON.stringify(message);
 
     [...clients.keys()].forEach((client) => {
       client.send(outbound);
     });
+
   });
+
   ws.on("close", () => {
     clients.delete(ws);
   });
+  
 });
 
 function uuidv4() {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+  return 'xxxxxxxx-xxxx-rled-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
     var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
     return v.toString(16);
   });
